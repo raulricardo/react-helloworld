18bdf71b9ee400a9c563aa1e823ba66d1dac2067
Raúl Amaya <raulricardo21@gmail.com>
2017-01-30 14:47:41 -0500

    Fetching some data

--- src/components/character_list/character_list.js
+++ src/components/character_list/character_list.js
@@ -4,19 +4,24 @@ import './character_list.scss';
 
 
 class CharacterList extends React.Component {
+  constructor() {
+    super();
+    this.state = {items: []}
+  }
+  componentWillMount(){
+    fetch('http://swapi.co/api/people/?format=json')
+      .then( response => response.json() )
+      .then( ({results: items}) => this.setState({items}))
+  }
   render() {
+    let items = this.state.items;
+
     return (
       <ul className="list">
         <h3 className="list__title">Star Wars Characters</h3>
-        <li className="list__item">
-          list item
-        </li>
-        <li className="list__item">
-          list item
-        </li>
-        <li className="list__item">
-          list item
-        </li>
+
+        {items.map(item =>
+          <li key={item.name}> {item.name} </li>)}
       </ul>
     )
   }
