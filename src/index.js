ed15d4dbf75dba0fd6b3199528a9c83b321a591a
Raúl Ricardo Amaya Sánchez <raulricardo21+trusty@gmail.com>
2017-02-02 23:17:15 -0500

    Adding thunk and logger

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './App';
-import reducers from './reducers';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import reducers from './reducers'
+import App from './App'
 
 import './index.css';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const logger = createLogger();
+const middleware = [ thunk, logger ]
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)>
+const store = createStore(
+  reducers,
+  applyMiddleware(...middleware)
+)
+
+render(
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
